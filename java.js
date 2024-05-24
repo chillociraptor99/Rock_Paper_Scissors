5215ab0928d19f3289f85020bee335ec46e7d54e
Chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-05-24 10:31:32 -0500

    Generate computer choice
    Generate computer's selection.

---
+++ java.js
@@ -0,0 +1,23 @@
+let computerChoice = "nothing"; // Computer's choice
+let playerChoice = "nothing";   // Player's choice
+let computerCnum = 0;           // Player's choice as a number
+let playerCnum = 0;             // Computer's choice as a number
+let playerScore = 0;            // Player's number of wins
+let computerScore = 0;          // Computer's number of wins
+
+// Generate random whole number 0-2 and assign it to a valid choice
+function getComputerChoice(cNum) {
+    computerCnum = Math.floor(Math.random () * 3);
+    if (computerCnum === 0) {
+        computerChoice = "Rock";
+    } else if (computerCnum === 1) {
+        computerChoice = "Scissors";
+    } else {
+        computerChoice = "Paper";
+    }
+    console.log(computerCnum);
+    console.log(computerChoice);
+}
+
+getComputerChoice();
+
