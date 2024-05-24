207460a48573f4918cc6ce07a0bc72bd3c859ef1
chillociraptor <169479308+chillociraptor99@users.noreply.github.com>
2024-05-24 12:53:55 -0500

    Cleanup code
    Cleaned up unneeded comments and code.

--- java.js
+++ java.js
@@ -7,7 +7,7 @@ let roundScore = 0;             // Who won the round, 0 for PC, 1 for player
 let round = 0;                  // Round, set to 0 at game start
 
 
-function playGame(rnd) {
+function playGame() {
     do {
         // Generate random whole number 0-2 and assign it to a valid choice
         function getComputerChoice() {
@@ -19,22 +19,18 @@ function playGame(rnd) {
             } else {
                 computerChoice = "PAPER";
             }
-        //    console.log(computerCnum); Uncomment to debug
-        //   console.log(computerChoice); Uncomment to debug
         }
-    getComputerChoice();
+        getComputerChoice();
 
         // Get player's choice via prompt 
         function getPlayerChoice(pChoice) {
             pChoice = window.prompt("Rock, Paper, Scissors?");
             playerChoice = pChoice.toUpperCase();
         }
-    getPlayerChoice();
+        getPlayerChoice();
 
         // Compare player choice to computer choice
         function playRound(humanChoice, pcChoice) {
-        //    console.log(computerChoice); Uncomment to debug
-        //   console.log(playerChoice); Uncomment to debug
             if (computerChoice === "ROCK" && playerChoice === "PAPER") {
                 console.log("You win!");
                 roundScore = 1;
@@ -85,8 +81,6 @@ function playGame(rnd) {
             } else if (roundScore === 1) {
                 ++playerScore;
             } else console.log("Error. What did you do?");
-//            console.log("Your score is " + playerScore + "!");
- //           console.log("The computer's score is " + computerScore + "!");
             if (playerScore > computerScore) {
                 console.log("You're winning!");
             } else if (computerScore > playerScore) {
@@ -99,8 +93,7 @@ function playGame(rnd) {
 }
 
     // Check for game end condition and print final scores
-    function gameOver(ovr) {
-    //    console.log("checking round: " + round); Uncomment to debug
+    function gameOver() {
         if (round === 6) {
             console.log("Game over! Your final score was " + playerScore + ". The computer's final score was " + computerScore + ".");
             if (playerScore > computerScore) {
